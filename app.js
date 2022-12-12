d70632d06506653a9259618afb3d4da1bb8bdd0a
uwishema11 <uwishemaceline4@gmail.com>
2022-12-12 14:41:03 +0200

    register a new user

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+
 const express =require('express');
 const morgan =require('morgan');
+const userRouter= require('./routers/userRouter');
+const AppError= require ('./utils/appError');
+
+
 const app=express();
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan('dev'))
+
+app.use('/api/v1/users',userRouter);
 
 
+app.all('*',(req,res,next)=>{
+    
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+})
 
+
+ 
 module.exports=app
